6c9a7f9dfa07b7d5f515f3ba4fcead82d5233924
Nghesqui <Nghesqui@eppc0678>
2018-03-29 13:17:27 +0200

    version update

--- LeagueInformerClient/src/main/java/net/nilsghesquiere/util/ProgramConstants.java
+++ LeagueInformerClient/src/main/java/net/nilsghesquiere/util/ProgramConstants.java
@@ -1,7 +1,7 @@
 package net.nilsghesquiere.util;
 
 public class ProgramConstants {
-	public static final String CLIENT_VERSION = "0.8.3";
+	public static final String CLIENT_VERSION = "0.8.4";
 	public static final String INI_NAME = "settings.ini";
 	public static final String INFERNAL_PROG_NAME = "Infernal-Start.exe" ;
 	public static final String WEBSERVER = "infernalbot-manager.herokuapp.com" ;
