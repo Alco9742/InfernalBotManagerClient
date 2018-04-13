d743a29ff22022058020aa29de7e7f91b7a057e0
Nghesqui <Nghesqui@eppc0678>
2018-04-13 15:26:16 +0200

    client and server version to 0.9.2

--- src/main/java/net/nilsghesquiere/util/ProgramConstants.java
+++ src/main/java/net/nilsghesquiere/util/ProgramConstants.java
@@ -1,8 +1,8 @@
 package net.nilsghesquiere.util;
 
 public class ProgramConstants {
-	public static final String CLIENT_VERSION = "0.9.1";
-	public static final String SERVER_VERSION = "0.9.1";
+	public static final String CLIENT_VERSION = "0.9.2";
+	public static final String SERVER_VERSION = "0.9.2";
 	public static final String INI_NAME = "settings.ini";
 	public static final String INFERNAL_PROG_NAME = "Infernal-Start.exe" ;
 	public static final String LEGACY_LAUNCHER_NAME = "Infernal Launcher.exe";
