bcd51db314e1d11d9828d6290b6187858bc8123f
Nghesqui <Nghesqui@eppc0678>
2018-06-20 16:26:02 +0200

    add clientaction to index

---
+++ src/main/java/net/nilsghesquiere/util/enums/ClientAction.java
@@ -0,0 +1,11 @@
+package net.nilsghesquiere.util.enums;
+
+public enum ClientAction {
+	RUN,
+	SAFESTOP, 
+	SAFESTOP_RESTART_INFERNAL,
+	SAFESTOP_REBOOT,
+	STOP,
+	STOP_REBOOT,
+	DISCONNECTED //Client only
+}
